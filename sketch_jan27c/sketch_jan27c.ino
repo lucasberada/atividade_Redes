be87cd2a92279292d0260d23adb6b4004eda465a
Lucas Santos Costa <54954484+lucasberada@users.noreply.github.com>
2021-02-02 16:36:54 -0200

    adicionados  comentarios
    adicionado comentarios

--- sketch_jan27c/sketch_jan27c.ino
+++ sketch_jan27c/sketch_jan27c.ino
@@ -1,3 +1,4 @@
+/*bibliotecas*/
 #include <PubSubClient.h>
 
 #include <UIPEthernet.h>
@@ -5,8 +6,13 @@
 #include <SPI.h>
 
 byte mac[] = {0x48, 0x67, 0x4B, 0x00, 0x05, 0xB5};
+/*variavel mensagen*/
 bool message;
+
+/*variavel inteira pino2 de valor 2*/
 int pino2 = 2;
+
+/*variavel estado do sensor*/
  bool estado_sensor;
  
   EthernetClient client;
@@ -14,18 +20,22 @@ int pino2 = 2;
 
 void setup() {
 
+/*lê o endereco mac*/
   Ethernet.begin(mac);
 
   Serial.begin(9600);
   
   mqttClient.setServer("3.82.94.158",1883);
 
+ /*retorna o ip do arduino*/
   Serial.print("o ip do arduino e:");
   Serial.println(Ethernet.localIP());
   
+  /*retorna a masara de rede*/
   Serial.print("a mascara de rede do arduino e:");
   Serial.println(Ethernet.subnetMask());
   
+  /*retorna o gateway*/
   Serial.print("o gateway do arduino e:");
   Serial.println(Ethernet.gatewayIP());
 
@@ -45,9 +55,11 @@ void loop() {
   
   estado_sensor = digitalRead(pino2);
  
+ /*se o estado do sensor for = a 0 ele executa esse trecho*/
   if(estado_sensor == 0){
   message = mqttClient.publish("lucasSantos_t","esta fechado");
   }else{
+    /*caso nao seja  = a 0 ele executa esse*/
    message = mqttClient.publish("lucasSantos_t","esta aberto");
    }
    
